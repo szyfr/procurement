97ad4c0a59187040da26d86c68c8f59b518b5303
Ren <neraj.mendoza@yahoo.com>
2026-08-19 10:05:22 +0800

    chore: secure false for now

--- src/modules/auth/dal/auth-cookies.ts
+++ src/modules/auth/dal/auth-cookies.ts
@@ -23,7 +23,7 @@ import { CSRF_COOKIE, SESSION_COOKIE } from "@/modules/auth/constants";
 const cookieOptions = {
   httpOnly: true,
   sameSite: "lax",
-  secure: process.env.NODE_ENV === "production",
+  secure: false,
   path: "/",
 } as const;
 
